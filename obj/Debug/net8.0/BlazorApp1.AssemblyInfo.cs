0c71afc5c34c14482a6aa6d60d284d596091d0c3
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-08-05 23:32:49 +0300

    add ef core and postgres package

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8762c98934e0d1fff2b56bf3510bd7d00bbcf3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c31050c2c0ffe014e7d2a313d3b2a9a478c1d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
