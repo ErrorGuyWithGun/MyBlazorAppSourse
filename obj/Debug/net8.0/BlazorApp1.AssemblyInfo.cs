ef4bc2c5c174f5f3b5c0f35aa671741581ca41e7
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-09-02 17:36:45 +0300

    Try to deploy

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b912cecb2ad7b7f1f4a4876312a460bcab2cfad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ab112d0086a0a4c0758b7a279e27dcfdbf355b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
