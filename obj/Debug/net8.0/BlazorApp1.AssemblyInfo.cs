7ff64f0f3ef59177b434714f6ea11da90b1ddb46
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-09-02 16:31:41 +0300

    Tty fix deploy

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77cac4ab6b3864a36cfa2929d29b2dbf015315f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b912cecb2ad7b7f1f4a4876312a460bcab2cfad")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
