1e6343a7e3ea947660d0848d384541d72c5dc21b
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-09-02 21:43:24 +0300

    Fix Adaptive In Admin Panel

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485440363f80efe5c991cb3aab1a513059688914")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ba98b156225394e60c1ef438b1c3a4b4e465f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
