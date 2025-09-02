97ba98b156225394e60c1ef438b1c3a4b4e465f1
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-09-02 20:27:54 +0300

    Redacted interface in User List

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e959c707382aff7d83783b7a0c23225a4d589cde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485440363f80efe5c991cb3aab1a513059688914")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
