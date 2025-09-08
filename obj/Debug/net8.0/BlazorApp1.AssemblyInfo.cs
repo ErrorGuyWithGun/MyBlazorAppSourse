7a388bb7cd4c412fec980823ee1c0fa26462ea83
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-09-08 19:12:59 +0300

    Hot Fix

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75edbd51b7c78a479c78d240701cfa54f93a779f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38eeba4421aa7e2b4b60d1c4dc2dcc094276ac6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
